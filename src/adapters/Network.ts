50374ec0229619823387a96a9cdfd59b383fdd94
Giuseppe Lippolis <workgroupengineering@outlook.com>
2020-12-02 17:39:05 +0100

    fixes error when invoke close

--- src/adapters/Network.ts
+++ src/adapters/Network.ts
@@ -12,19 +12,23 @@ export default class Network extends Adapter {
     private device: Socket;
     private retries: number;
     private connected: boolean;
+    private forceClose: boolean;
 
     constructor(address: string, port: number = 9100, retries: number = 0) {
         super();
         this.device = new Socket();
         this.retrying = false;
+        this.forceClose = false;
         this.retries = 0;
-        this.options = {
-            address,
+        this.options = {address,
             port
         };
 
         this.device.on("close", () => {
             this.connected = false;
+            if (this.forceClose) {
+                return;
+            }
             if (this.retrying && (retries === 0 || this.retries < retries)) {
                 this.retries++;
                 setTimeout(() => {
@@ -42,6 +46,7 @@ export default class Network extends Adapter {
     public async open(): Promise<void> {
         return new Promise<void>(resolve => {
             this.retrying = true;
+            this.forceClose = false;
             this.device.connect(this.options.port, this.options.address);
             this.device.on("connect", () => {
                 this.retrying = false;
@@ -62,6 +67,7 @@ export default class Network extends Adapter {
         this.throwIfNeeded();
         this.retrying = false;
         this.connected = false;
+        this.forceClose = true;
         this.device.destroy();
     }
 
