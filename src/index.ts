294149f817949e85197ff82d87538d07a79a8aad
Giuseppe Lippolis <workgroupengineering@outlook.com>
2020-12-28 18:13:21 +0100

    Add export Image

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import * as Adapters from "./Adapters";
 import * as Commands from "./Commands";
+import * as Image from "./Image";
 import Printer from "./Printer";
 
 export {
     Adapters,
     Commands,
+    Image,
     Printer
 };
